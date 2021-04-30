320890f2838bc0f14703c0ee66da489446b42eff
katyacano <katyacano@gmail.com>
2021-04-30 12:44:48 +0000

    Added About page

--- home/views.py
+++ home/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ A view to return the About page """
+
+    return render(request, 'home/about.html')
