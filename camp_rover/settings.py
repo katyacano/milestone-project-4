7477c16ac6fc5afa0fb809ea24d7d8b335ba9cbd
katyacano <katyacano@gmail.com>
2021-05-25 13:07:35 +0000

    Completed fix to db issues

--- camp_rover/settings.py
+++ camp_rover/settings.py
@@ -119,22 +119,22 @@ WSGI_APPLICATION = 'camp_rover.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if 'DATABASE_URL' in os.environ:
-#    DATABASES = {
-#        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#    }
-# else:
-#    DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.sqlite3',
-#            'NAME': BASE_DIR / 'db.sqlite3',
-#        }
-#    }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-DATABASES = { 
-    'default': dj_database_url.parse('postgres://yxkzcchubxlyge:9d7757665f57a320610371baa6b512ddb510add0348ec8373bb7e9493f5ad1db@ec2-3-233-7-12.compute-1.amazonaws.com:5432/deigrohik6iu2r')
-
-}
+# DATABASES = {
+#     'default': dj_database_url.parse('postgres://yxkzcchubxlyge:9d7757665f57a320610371baa6b512ddb510add0348ec8373bb7e9493f5ad1db@ec2-3-233-7-12.compute-1.amazonaws.com:5432/deigrohik6iu2r')
+#
+#}
 
 
 # Password validation
