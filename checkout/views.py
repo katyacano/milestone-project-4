c4bf4d236ea124783df505894c4d5e731adacabe
katyacano <katyacano@gmail.com>
2021-05-28 11:37:14 +0000

    Fixing linting errors- part 2

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.shortcuts import (
+        render, redirect, reverse, get_object_or_404, HttpResponse)
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
@@ -79,7 +80,8 @@ def checkout(request):
                             order_line_item.save()
                 except Product.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your bag wasn't found in our database. "
+                        "One of the products in your bag"
+                        "wasn't found in our database. "
                         "Please call us for assistance!")
                     )
                     order.delete()
